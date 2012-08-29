7dbcad0fb4c3a71f49d2c82bf7e390f48b956a93
rgruby <rgruby@raleygroup.com>
2012-08-29 13:44:52 -0400

    Properly update 'active' css class on nav-bar li items.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,13 @@ module ApplicationHelper
       "#{base_title} | #{page_title}"
     end
   end
+
+  # Build <li> nav link element with css 'active' class set correctly.
+  def nav_link(link_text, link_path)
+    class_name = current_page?(link_path) ? 'active' : ''
+
+    content_tag(:li, :class => class_name) do
+      link_to link_text, link_path
+    end
+  end
 end
